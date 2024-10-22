676efd6e4dea360025cbcfe16fd4d3c65fb6dc0d
Java Tech Solutions <shilc8332@gmail.com>
2024-10-22 11:11:22 +0530

    Added display property

---
+++ property-booking-app/src/app/components/view-property/view-property.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { IntegrationService } from '../../services/integration.service';
+import { Property } from '../../models/property';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-view-property',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-property.component.html',
+  styleUrl: './view-property.component.css'
+})
+export class ViewPropertyComponent {
+
+  properties: Property[] = [];
+  sanitizer = inject(DomSanitizer);
+
+  constructor(private integration: IntegrationService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllProperty();
+  }
+
+  getAllProperty() {
+    this.integration.getAllProperety().subscribe({
+      next: (res) => {
+        this.properties = res;
+
+        for (let r of res) {
+          r.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
+            `data:image/png;base64, ${r.photo}`
+          );
+        }
+      }, error: (err) => {
+        console.log("Error response:" + err);
+      }
+    })
+  }
+
+  bookProperty() {
+    
+  }
+
+}
